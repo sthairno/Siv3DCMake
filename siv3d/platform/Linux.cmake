1f8eba281b6a7d9740b0f1ebb1ae0b01b317f31b
sthairno <45668272+sthairno@users.noreply.github.com>
2026-06-28 15:43:26 +0900

    create symlink for reference

--- siv3d/platform/Linux.cmake
+++ siv3d/platform/Linux.cmake
@@ -1,5 +1,27 @@
 find_package(Siv3D REQUIRED)
-get_target_property(SIV3D_INCLUDE_DIRS Siv3D::Siv3D INTERFACE_INCLUDE_DIRECTORIES)
+get_target_property(SIV3D_SYSTEM_INCLUDE_DIRS Siv3D::Siv3D INTERFACE_INCLUDE_DIRECTORIES)
+
+set(SIV3D_ROOT "${CMAKE_CURRENT_LIST_DIR}/../sdk")
+set(SIV3D_INCLUDE_DIR "${SIV3D_ROOT}/include")
+foreach(include_dir IN LISTS SIV3D_SYSTEM_INCLUDE_DIRS)
+    if(EXISTS "${include_dir}/Siv3D.hpp")
+        set(SIV3D_SYSTEM_INCLUDE_DIR "${include_dir}")
+        break()
+    endif()
+endforeach()
+
+if(NOT SIV3D_SYSTEM_INCLUDE_DIR)
+    message(FATAL_ERROR
+        "Failed to find Siv3D include directory: ${SIV3D_SYSTEM_INCLUDE_DIRS}")
+endif()
+
+# Create symlink from SIV3D_SYSTEM_INCLUDE_DIR to SIV3D_INCLUDE_DIR
+file(REMOVE_RECURSE "${SIV3D_INCLUDE_DIR}")
+file(CREATE_LINK
+    "${SIV3D_SYSTEM_INCLUDE_DIR}"
+    "${SIV3D_INCLUDE_DIR}"
+    SYMBOLIC
+)
 
 function(_siv3d_platform_add_resources target resource_paths resource_types)
     list(LENGTH resource_paths path_count)
@@ -32,4 +54,4 @@ function(_siv3d_platform_add_resources target resource_paths resource_types)
 endfunction()
 
 message(STATUS "Configured Siv3D SDK [Linux]")
-message(STATUS "  Include: ${SIV3D_INCLUDE_DIRS}")
+message(STATUS "  Include: ${SIV3D_SYSTEM_INCLUDE_DIR} -> ${SIV3D_INCLUDE_DIR}")
